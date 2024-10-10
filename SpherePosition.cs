bf77fe4ac51832d5d4b23facb88c30eebd05de77
Alvaro2560 <alvaro.fontenla.33@ull.edu.es>
2024-10-10 11:14:25 +0100

    third script

---
+++ SpherePosition.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpherePosition : MonoBehaviour
+{
+  void Start()
+  {
+    Debug.Log("Posición de la esfera: " + transform.position);
+  }
+}
